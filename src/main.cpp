5ff39627a84657da5f548b67bdd288c0067964cb
tieuma113 <dk.assassins.9x@gmail.com>
2022-04-25 17:20:05 +0700

    xu ly gps

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+#include <string.h>
+
+#define BG95    Serial1
+
+String kinhDo, viDo;
+
+bool askForLocation();
+void sendIt(String atCommand);
+String receiveIt();
+String checkGPSLocation();
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,4 +19,59 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+}
+
+
+
+
+
+bool askForLocation(){
+  String receivedMessage;
+  sendIt("AT+QGPSLOC=0");
+  String received = checkGPSLocation();
+  if (received.length() > 0) {
+    viDo = received.substring(0, received.indexOf(","));
+    kinhDo = received.substring(1 + received.indexOf(","));
+    return true;
+  }else{
+    return false;
+  }
+
+}
+
+void sendIt ( String atCommand ) 
+{
+	atCommand += "\r\n";
+  BG95.print(atCommand);
+}
+
+String checkGPSLocation(){
+  int timeOut = 300;					// From BG96 Quectel AT Commands Manual
+	String response;
+	String received = "";
+	bool ok = false;
+	unsigned long startTime = millis();	// Take time at start for time out
+
+	while ( !ok && ((millis()-startTime) <= (unsigned long) timeOut) ) {
+    response = receiveIt();
+    if (response.indexOf("\r\nOK\r\n") >=0 ){
+      if(response.indexOf("\r\n+QGPSLOC:")>=0){
+        received = response.substring(10,32);
+      }
+    }
+  }
+  return received;
+}
+
+String receiveIt ( ) 
+{
+	String received;
+
+	// Save data received from BG96 module
+	while (BG95.available()) {
+		received += (char)BG95.read ();
+	}
+
+	return received;					// Return string received
 }
