63f8813a9e0f7cf69da12b96dd1d11a398b1ce89
tieuma113 <dk.assassins.9x@gmail.com>
2022-04-27 01:05:18 +0700

    hoan thanh chuoi string

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <string.h>
 
-#define BG95    Serial1
+using namespace std;
+
+#define BG95    Serial2
 
 String kinhDo, viDo;
 
@@ -10,6 +12,7 @@ void sendIt(String atCommand);
 String receiveIt();
 String checkGPSLocation();
 void GPSsetup();
+bool checkRespForOk( int timeOut ) ;
 
 
 
@@ -17,38 +20,69 @@ void GPSsetup();
 void setup() {
   // put your setup code here, to run once:
   GPSsetup();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (askForLocation()){
-    Serial.println(kinhDo);
-    Serial.println(viDo);
+    Serial.println("kinh do = " + kinhDo);
+    Serial.println("Vi do = "+ viDo);
   }else{
     Serial.println("fail");
   }
+  // bool flag = false;
+  // while(!flag){
+  //   sendIt("AT");
+  //   flag = checkRespForOk(30);
+  //   Serial.println("reconnect");
+  // }
+  // Serial.println("connect");
+  // sendIt("AT+QGPSLOC=0,36");
+  // String data = checkGPSLocation();
+  // Serial.println(data);
+  Serial.println("++++++++++++++");
+  // sendIt("AT+QGPSLOC=0,36");
+  // String data = receiveIt();
+  // Serial.println(data);
+  delay(5000);
+
 }
 
 
 void GPSsetup() {
+  bool flag = false;
   BG95.begin(115200);
   while(!BG95.available());
   Serial.begin(9600);
   while(Serial.available());
-  sendIt("AT+QGPSCFG=\"outport\",\"uartnmea\"");
-  Serial.println(receiveIt());
-  sendIt("AT+QGPS=1");
-  Serial.println(receiveIt());
+  while(!flag){
+    sendIt("AT");
+    flag = checkRespForOk(30);
+    Serial.println("reconnect");
+  }
+  flag = false;
+  while(!flag){
+    sendIt("AT+QGPSCFG=\"outport\",\"uartnmea\"");
+    flag = checkRespForOk(30);
+    Serial.println("reconnect");
+  }
+  flag = false;
+  while(!flag){
+    sendIt("AT+QGPS=1");
+    flag = checkRespForOk(30);
+    Serial.println("reconnect");
+  }
 }
 
 
 bool askForLocation(){
   String receivedMessage;
-  sendIt("AT+QGPSLOC=0,360");
+  sendIt("AT+QGPSLOC=0,36");
   String received = checkGPSLocation();
   if (received.length() > 0) {
-    viDo = received.substring(0, received.indexOf(","));
-    kinhDo = received.substring(1 + received.indexOf(","));
+    viDo = received.substring(0, 2) + "*" + received.substring(3,8) + "'" + received.substring(9, received.indexOf(","));
+    kinhDo = received.substring(1 + received.indexOf(","), 4 + received.indexOf(",")) + "*" + received.substring(5 + received.indexOf(","), 10 + received.indexOf(",")) + "'" + received.substring(11 + received.indexOf(","));
     return true;
   }else{
     return false;
@@ -62,7 +96,7 @@ void sendIt ( String atCommand )
 }
 
 String checkGPSLocation(){
-  int timeOut = 360;					// From BG95 Quectel AT Commands Manual
+  int timeOut = 60;					// From BG95 Quectel AT Commands Manual
 	String response;
 	String received = "";
 	bool ok = false;
@@ -72,7 +106,8 @@ String checkGPSLocation(){
     response = receiveIt();
     if (response.indexOf("\r\nOK\r\n") >=0 ){
       if(response.indexOf("\r\n+QGPSLOC:")>=0){
-        received = response.substring(10,32);
+        response = response.substring(response.indexOf(":"));
+        received = response.substring(13,35);
       }
     }
   }
@@ -89,4 +124,22 @@ String receiveIt ( )
 	}
 
 	return received;					// Return string received
+}
+
+bool checkRespForOk( int timeOut ) 
+{
+	String response;
+	bool ok = false;
+	bool error = false;
+	unsigned long startTime = millis();	// Take time at start for time out
+
+	// Check every char string received for "OK" response
+	while ( !error && !ok && ((millis()-startTime) <= (unsigned long) timeOut) ) {
+		response += receiveIt();
+    Serial.println(response);
+		ok = (response.indexOf("\r\nOK\r\n") >= 0);	// Check if "OK" is in the response
+		error = (response.indexOf("\r\nERROR\r\n") >= 0);	// Check if "ERROR" is in response
+	}
+
+	return ok;
 }
